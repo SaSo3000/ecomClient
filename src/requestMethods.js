9861f6b22d741290fc53e71f4a45d4271f09dea8
SaSo3000 <saunak3000@gmail.com>
2023-12-24 01:51:33 -0800

    change

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ecomback-itiq.onrender.com/api/"
+const BASE_URL = "https://ecom-api-gules-pi.vercel.app/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjNlMjY5ZDQ1MDRmMDI5YzUyYmI0YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMzEwNjk1MCwiZXhwIjoxNzAzMzY2MTUwfQ.rL5VoWX7p9mNZSlDYTlA7N4GLQm-33qPek3ukFMa3jc";
 export const publicRequest = axios.create({
     baseURL : BASE_URL,
